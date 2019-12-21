fc1f8356dfc7d6e73f92a94806c3f0eb869b88a2
gogiakush <49599534+gogiakush@users.noreply.github.com>
2019-12-21 11:27:21 -0800

    Create Variables.java

---
+++ week1/Variables.java
@@ -0,0 +1,23 @@
+public class Variables
+{
+	public static void main(String [] args)
+	{
+		int x = 5; 
+		//x = five;
+		//x = 5.0;
+		
+		System.out.println(x);
+		
+		String str = "five";
+		//str = "5";
+		//str = "5.0";
+		
+		System.out.println(str);
+		
+		double y = 5.0;
+		//y = 5;
+		//y = "5";
+		
+		System.out.println(y);
+	}
+}
