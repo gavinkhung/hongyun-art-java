abc63c31f7dec6216393eb63ace4998293ecbff6
gogiakush <49599534+gogiakush@users.noreply.github.com>
2019-12-21 11:33:15 -0800

    Update ForLoop.java

--- week1/ForLoop.java
+++ week1/ForLoop.java
@@ -1,4 +1,34 @@
 public class ForLoop
 {
+	public static void main(String [] args)
+	{
+		ForLoop kb = new ForLoop();
+		kb.forLoop1();
+		//kb.forLoop2();
+		//kb.forLoop3();
+	}
 	
+	public void forLoop1()
+	{
+		for(int i = 1; i <= 10; i++)
+		{
+			System.out.println(i);	
+		}	
+	}
+	
+	public void forLoop2()
+	{
+		for(int j = 0; j < 10; j++)
+		{
+			System.out.println("a");
+		}
+	}
+	
+	public void forLoop3()
+	{
+		for(int k = 0; k > 1; k++)
+		{
+			System.out.println(k);	
+		}
+	}
 }
