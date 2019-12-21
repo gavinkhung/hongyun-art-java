ed525556c9442b5dc7102e199fba7e23778c5f3c
gogiakush <49599534+gogiakush@users.noreply.github.com>
2019-12-21 15:02:56 -0800

    Update Arrays.java

--- week1/Arrays.java
+++ week1/Arrays.java
@@ -1,1 +1,11 @@
-
+public class Arrays
+{
+	public static void main(String [] args)
+	{
+		int [] numbers = new int[5];
+		for(int i = 0; i < numbers.length; i++)
+		{
+			numbers[i] = i + 1;	
+		}
+	}
+}
