9a29e0d82ee07abc1daea6c1539e4bcaa47218a9
Gavin Hung <gavinkhung@gmail.com>
2019-12-22 14:42:41 -0800

    week10

--- week12/Constants.java
+++ week12/Constants.java
@@ -2,7 +2,7 @@ public class Constants {
 	
 	public static final int WIDTH = 1200, HEIGHT = 800;
 	
-	public static final String projectDirectorySerialized = "week12/";
+	public static final String projectDirectorySerialized = "serialized/";
 	public static final String fileName = "name.txt";
 	
 }
