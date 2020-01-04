ec84b02951f5b4230943ed69481253951aadb241
gogiakush <49599534+gogiakush@users.noreply.github.com>
2020-01-03 23:07:06 -0800

    Update Dog.java

--- week2/projects/Dog.java
+++ week2/projects/Dog.java
@@ -1,4 +1,28 @@
-public class Dog
+public class Aninal
 {
+  private String name;
+  private int age;
+  private String favoriteFood;
   
+  public Dog(String name, int age, String favoriteFood)
+  {
+    this.name = name;
+    this.age = age;
+    this.favoriteFood = favoriteFood;
+  }
+  
+  public String getName()
+  {
+   return name; 
+  }
+  
+  public int getAge()
+  {
+   return age; 
+  }
+  
+  public String getFood()
+  {
+   return favoriteFood; 
+  }
 }
